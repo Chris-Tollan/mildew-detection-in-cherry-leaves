cb345a8a4b955c49fd4d08e7b8a4e9319ae16509
Chris Tollan <chrisytollan@googlemail.com>
2024-04-15 09:38:16 +0000

    load page scripts, create app instance and add app pages - app.py

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_cherry_leaf_visualizer import page_cherry_leaf_visualizer_body
+from app_pages.page_mildew_infection_detector import page_mildew_infection_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
+
+app = MultiPage(app_name="Mildew Infection Detector")  # Create an instance of the app
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Cherry Leaves Visualiser", page_cherry_leaf_visualizer_body)
+app.add_page("Mildew Infection Detector", page_mildew_infection_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+# Run the app
+app.run()
