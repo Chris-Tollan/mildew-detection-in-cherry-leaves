c6268e63d2ba3cd429e53a653b6f5d63f264a022
Chris Tollan <chrisytollan@googlemail.com>
2024-04-16 09:32:10 +0000

    import packages, create leaf visualizer func - page_cherry_leaf_visualizer.py

---
+++ app_pages/page_cherry_leaf_visualizer.py
@@ -0,0 +1,65 @@
+# Import packages and libraries
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+import itertools
+import random
+
+
+def page_leaf_visualizer_body():
+    st.write("### Cherry Leaf Visualizer")
+    st.info(
+        f"The client is interested in the Visual differentiation"
+        f"of healthy cherry leaves to that of a cherry leaf"
+        f"that is infected with powdery mildew.")
+
+    version = 'v1'
+    if st.checkbox("Difference between average and variability image"):
+
+        avg_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
+        avg_powdery_mildew = plt.imread(
+            f"outputs/{version}/avg_var_powdery_mildew.png"
+        )
+
+        st.warning(
+            f"* We notice that cherry leaves infected with powdery"
+            f"mildew do present subtle light marks on them them"
+            f"which can not be seen on healthy leaves"
+            f"however the average and variability images did not show "
+            f"patterns where we could intuitively differentiate one from another."
+        )
+
+        st.image(avg_healthy, caption='Healthy Leaf - Average & Variability')
+        st.image(avg_powdery_mildew, caption='Mildew Infected Leaf - Average & Variability')
+        st.write("---")
+
+    if st.checkbox("Differences between average mildew-infected and healthy leaves"):
+        diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
+
+        st.warning(
+            f"* We notice this study didn't show "
+            f"patterns where we could intuitively differentiate "
+            f"one to another.")
+        st.image(
+            diff_between_avgs, caption='Difference between average images'
+        )
+
+    if st.checkbox("Image Montage"):
+        st.write(
+            "* To refresh the montage, click on the 'Create Montage' button"
+        )
+        my_data_dir = 'inputs/cherry_leaves_dataset/cherry-leaves'
+        labels = os.listdir(my_data_dir + '/validation')
+        label_to_display = st.selectbox(
+            label="Select label", options=labels, index=0
+        )
+        if st.button("Create Montage"):
+            image_montage(dir_path=my_data_dir + '/validation',
+                          label_to_display=label_to_display,
+                          nrows=8, ncols=3, figsize=(10, 25))
+        st.write("---")
