d38d9e8b03f295bbf6231b8285c5390dd6f2a646
Chris Tollan <chrisytollan@googlemail.com>
2024-04-18 10:19:08 +0000

    correct grammer errors - project_hypothesis.py

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -15,8 +15,8 @@ def page_project_hypothesis_body():
 
     st.success(
         f"### Validation:\n\n"
-        f"By display images of infected and healthy leaves we can visually "
-        f"compare them in order to establish differences in the leaves"
+        f"By displaying images of infected and healthy leaves we can visually "
+        f"compare them in order to establish differences in the leaves.\n"
         f"This is done by creating an image montage to conduct the comparison of "
         f"infected and healthy leaves.\n"
         f'From analysis of the montage it can be seen that infected leaves '
